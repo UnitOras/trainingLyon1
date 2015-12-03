0287a42da543b201ea1dd9269c9848791732cbe7
UnitOras <olivier.rastello@univ-lyon1.fr>
2015-12-03 11:34:52 +0100

    Scene 2

---
+++ 3DGeoSciences/Assets/SetButtonAlpha.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetButtonAlpha : MonoBehaviour
+{
+	public void SetAplpha (float alpha)
+    {
+        Color newColor = GetComponent<UnityEngine.UI.Image>().color;
+        newColor.a = alpha;
+        GetComponent<UnityEngine.UI.Image>().color = newColor;
+    }
+}
