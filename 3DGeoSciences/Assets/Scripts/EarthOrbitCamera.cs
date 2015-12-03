3108b030ed2fbab72d96c5717972b6ab4f054183
UnitOras <olivier.rastello@univ-lyon1.fr>
2015-12-03 14:28:32 +0100

    14-28

---
+++ 3DGeoSciences/Assets/Scripts/EarthOrbitCamera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthOrbitCamera : MonoBehaviour
+{
+    public float distance = 1.2f;
+    public float _Sensitivity;
+
+    float _currentRotationRight = 0f;
+    float _currentRotationUp = 0f;
+    
+    void Start ()
+    {
+        Rotate(_currentRotationRight, _currentRotationUp);
+    }
+
+
+	void Update ()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _currentRotationRight += Input.GetAxis("Mouse Y") * _Sensitivity;
+            _currentRotationUp += Input.GetAxis("Mouse X") * _Sensitivity;
+            Rotate( _currentRotationRight, _currentRotationUp);
+
+        }
+        	
+	}
+
+    void Rotate(float right, float up)
+    {
+        Quaternion rotation = Quaternion.Euler(-right, up, 0f);
+        transform.rotation = rotation;
+        Vector3 percheASelfie = new Vector3(0f, 0f, -distance);
+        transform.position = rotation * percheASelfie;
+    }
+}
