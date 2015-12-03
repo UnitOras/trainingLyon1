509dea00dd4397ceee251e6f0bcf887f12170f79
UnitOras <olivier.rastello@univ-lyon1.fr>
2015-12-03 19:19:30 +0100

    1/2 fine

---
+++ 3DGeoSciences/Assets/Scripts/RayCastSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCastSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    private float lasttimeMouseButtonDown;
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lasttimeMouseButtonDown = Time.time;
+        }
+
+        if (Input.GetMouseButtonUp(0) && Time.time -lasttimeMouseButtonDown < 0.1f)
+        {
+            SpawnOnHit();
+        }
+        
+    }
+
+    void SpawnOnHit()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) == true)
+        {
+            GameObject go = (GameObject) Instantiate(prefab, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+        }
+	}
+}
